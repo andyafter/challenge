5de09ff9c8b9a1e01f6596cf3be9c4317355d926
andyafter <andyafter@gmail.com>
2015-07-31 12:39:03 +0800

    tested angular bootstrap

--- server/food/api/controllers/FoodctrlController.js
+++ server/food/api/controllers/FoodctrlController.js
@@ -9,34 +9,34 @@ var fs = require("fs");
 module.exports = {
 
     testAction: function(req, res){
-	res.send('hello world');
+      res.send('hello world');
     },
 
     anotherAction : another,
 
 
-    
-    index : function(req,res){
-	// this is to test a way to add html page but it seems to be more complicated
-	var emberApp = __dirname + '/../../assets/templates/index.html';
-	fs.exists(emberApp, function (exists) {
-	    if (!exists) {
-		return res.notFound('The requested file does not exist.');
-	    }
 
-	    fs.createReadStream(emberApp).pipe(res);
-	});
-	
+    index : function(req,res){
+      // this is to test a way to add html page but it seems to be more complicated
+      var emberApp = __dirname + '/../../assets/templates/index.html';
+      fs.exists(emberApp, function (exists) {
+          if (!exists) {
+        return res.notFound('The requested file does not exist.');
+          }
+
+          fs.createReadStream(emberApp).pipe(res);
+      });
+
     },
 
     recordData : function(req,res){
-	// take this as a logging proccedure
-	console.log("recordingData");
-	
+      // take this as a logging proccedure
+      console.log("recordingData");
+
     }
-	
+
 };
 
-function another(req,res){
-	console.log("andyafter");
+    function another(req,res){
+      console.log("andyafter");
     }
---
+++ server/food/assets/js/public/BStest.js
@@ -0,0 +1,8 @@
+/**
+ * Created by andypan on 31/7/15.
+ */
+// this one is to test angular bootstrap
+
+
+var bsmod= angular.module('bsModule', ['ui.bootstrap.demo'])
+
